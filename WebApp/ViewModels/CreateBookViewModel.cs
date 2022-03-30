3f9961ce6b8bd5daf0d46028e2ce41d2f6ac1495
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-03-30 21:14:18 +0700

    Create and view list of book

---
+++ WebApp/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class CreateBookViewModel
+    {
+        public Book Book { get; set; } = null!;
+
+        [ValidateNever]
+        public SelectList Categories { get; set; } = null!;
+
+        public string Message { get; set; } = "Ditmemay asp.net core";
+    }
+}
