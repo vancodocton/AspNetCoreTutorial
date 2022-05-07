d9f8dd76c655f82a0bd3a9f791ff6cf83c56b5fa
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-05-07 22:04:35 +0700

    commit draft

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly ApplicationDbContext dbContext;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdminController(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await userManager.Users.ToListAsync();
+
+            return Json(users);
+        }
+    }
+}
---
+++ WebApp/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    public class CustomerController : Controller
+    {
+        [Authorize(Roles = "Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
